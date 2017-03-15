3698964d4024609e8180a4567869a8fd224f0cf6
dumebi <dikejude49@gmail.com>
2017-03-15 10:52:27 +0100

    marketplace init

---
+++ seed/ProductSeeder.js
@@ -0,0 +1,8 @@
+var Product =  require('../models/product');
+
+var product = new Product({
+    imagePath: 'https://upload.wikimedia.org/wikipedia/en/5/5e/Gothiccover.png',
+    title: 'Gothic Video Game',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium amet aspernatur consequatur debitis dicta, dolores doloribus ea exercitationem harum modi odio optio possimus rerum sapiente sed tempore ullam voluptatem. Quibusdam!!!',
+    price: 100
+});
